c6bb962660c2e82e6e9c0f418a081f3ef6d85b47
Khurasha <133712236+Khurasha@users.noreply.github.com>
2023-11-04 12:23:33 -0400

    Add files via upload

---
+++ form/Assignment4.js
@@ -0,0 +1,16 @@
+function Sumbit()
+{
+    var firstname = document.getElementById('firstname').value;
+    var middlename = document.getElementById('middlename').value;
+    var lastname = document.getElementById('lastname').value;
+    var email = document.getElementById('email').value;
+    var streetnumb = document.getElementById('streetnumb').value;
+    var streetname = document.getElementById('streetname').value;
+    var city = document.getElementById('city').value;
+    var postalcode = document.getElementById('postalcode').value;
+    var Phone = document.getElementById('Phone').value;
+    var Accomplishments = document.getElementById('Accomplishments').value;
+    var qualification = document.getElementById('qualification').value;
+    var certification = document.getElementById('certification').value;
+    var Experience = document.getElementById('Experience').value;
+}
